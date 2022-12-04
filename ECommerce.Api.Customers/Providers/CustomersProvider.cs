6ced6e93989d0b67d765d038d4e51d34e8af5c3e
[Chase <78990671+ChaseLipari@users.noreply.github.com>
2022-12-03 21:33:46 -0500

    Initial commit

---
+++ ECommerce.Api.Customers/Providers/CustomersProvider.cs
@@ -0,0 +1,83 @@
+﻿using AutoMapper;
+using ECommerce.Api.Customers.Db;
+using ECommerce.Api.Customers.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Customers
+{
+
+    public class CustomersProvider : ICustomersProvider
+    {
+        private readonly CustomerDbContext dbContext;
+        private readonly ILogger<CustomersProvider> logger;
+        private readonly IMapper mapper;
+
+        public CustomersProvider(CustomerDbContext dbContext, ILogger<CustomersProvider> logger, IMapper mapper)
+        {
+            this.dbContext = dbContext;
+            this.logger = logger;
+            this.mapper = mapper;
+
+            SeedData();
+        }
+
+        private void SeedData()
+        {
+            if (!dbContext.Customers.Any())
+            {
+                dbContext.Customers.Add(new Db.Customer() { Id = 1, Name = "Will", Address = "20, Inventory 100" });
+                dbContext.Customers.Add(new Db.Customer() { Id = 2, Name = "Dave", Address = "5, Inventory  200 "});
+                dbContext.Customers.Add(new Db.Customer() { Id = 3, Name = "John", Address = "150, Inventory  100" });
+                dbContext.Customers.Add(new Db.Customer() { Id = 4, Name = "Sloan", Address = "200, Inventory  2000" });
+                dbContext.SaveChanges();
+            }
+        }
+
+        public async Task<(bool IsSuccess, IEnumerable<Models.Customer> Customers, string ErrorMessage)> GetCustomersAsync()
+        {
+            try
+            {
+                var customers = await dbContext.Customers.ToListAsync();
+                if (customers != null && customers.Any())
+                {
+                    var result = mapper.Map<IEnumerable<Db.Customer>, IEnumerable<Models.Customer>>(customers);
+                    return (true, result, null);
+                }
+                return (false, null, "Not found");
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex.ToString());
+                return (false, null, ex.Message);
+
+            }
+        }
+
+        public async Task<(bool IsSuccess, Models.Customer Customer, string ErrorMessage)> GetCustomerAsync(int id)
+        {
+            try
+            {
+                var customer = await dbContext.Customers.FirstOrDefaultAsync(c => c.Id == id);
+                if (customer != null)
+                {
+                    var result = mapper.Map<Db.Customer, Models.Customer>(customer);
+                    return (true, result, null);
+                }
+                return (false, null, "Not found");
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex.ToString());
+                return (false, null, ex.Message);
+
+            }
+        }
+
+       
+    }
+}
---
+++ ECommerce.Api.Orders/Providers/OrdersProvider.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using ECommerce.Api.Orders.Db;
+using ECommerce.Api.Orders.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Orders.Providers
+{
+    public class OrdersProvider : IOrdersProvider 
+    {
+        private readonly OrdersDbContext dbContext;
+        private readonly ILogger logger;
+        private readonly IMapper mapper;
+
+        public OrdersProvider(OrdersDbContext dbContext, ILogger<OrdersProvider> logger, IMapper mapper)
+        {
+            this.dbContext = dbContext;
+            this.logger = logger;
+            this.mapper = mapper;
+
+            SeedData();
+        }
+
+        public async Task<(bool IsSuccess, IEnumerable<Models.Order> Orders, string ErrorMessage)> GetOrderAsync(int id)
+        {
+            try
+            {
+                var orders = await dbContext.Orders.Where(o => o.CustomerId == id).ToListAsync();
+                if (orders != null)
+                {
+                    var result = mapper.Map<IEnumerable<Db.Order>, IEnumerable<Models.Order>>(orders);
+
+                    return (true, result, null);
+                }
+                return (false, null, "Not found");
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex.ToString());
+                return (false, null, ex.Message);
+
+            }
+        }
+
+        private void SeedData()
+        {
+            if (!dbContext.Orders.Any())
+            {
+                dbContext.Orders.Add(new Db.Order() { Id = 1, CustomerId = 1, OrderDate = DateTime.Now, Total = 100, Items = null });
+                dbContext.Orders.Add(new Db.Order() { Id = 2, CustomerId = 1, OrderDate = new DateTime(2022, 11, 15), Total = 40.48m, Items = new List<Db.OrderItem> { new Db.OrderItem() { Id = 2, ProductId = 1, Quantity = 3, UnitPrice = 100 }, new Db.OrderItem() { Id = 6, ProductId = 2, Quantity = 5, UnitPrice = 9.99m } } });
+                dbContext.SaveChanges();
+            }
+        }
+    }
+}
